3b8605ec93b80ab5e9cae5dcdfb71e407eeb8e23
Ibai Gorordo <43162939+ibaiGorordo@users.noreply.github.com>
2019-09-22 20:07:38 +0900

    added program to read the azimuth heat map data

---
+++ heatMapDemo.py
@@ -0,0 +1,395 @@
+
+import serial
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Change the configuration file name
+configFileName = '1642config.cfg'
+CLIport = {}
+Dataport = {}
+byteBuffer = np.zeros(2**15,dtype = 'uint8')
+byteBufferLength = 0;
+
+# ------------------------------------------------------------------
+
+# Function to configure the serial ports and send the data from
+# the configuration file to the radar
+def serialConfig(configFileName):
+    
+    global CLIport
+    global Dataport
+    # Open the serial ports for the configuration and the data ports
+    
+    # Raspberry pi
+    CLIport = serial.Serial('/dev/ttyACM0', 115200)
+    Dataport = serial.Serial('/dev/ttyACM1', 921600)
+    
+    # Windows
+    #CLIport = serial.Serial('COM3', 115200)
+    #Dataport = serial.Serial('COM4', 921600)
+
+    # Read the configuration file and send it to the board
+    config = [line.rstrip('\r\n') for line in open(configFileName)]
+    for i in config:
+        CLIport.write((i+'\n').encode())
+        print(i)
+        time.sleep(0.01)
+        
+    return CLIport, Dataport
+
+# ------------------------------------------------------------------
+
+# Function to parse the data inside the configuration file
+def parseConfigFile(configFileName):
+    configParameters = {} # Initialize an empty dictionary to store the configuration parameters
+    
+    # Read the configuration file and send it to the board
+    config = [line.rstrip('\r\n') for line in open(configFileName)]
+    for i in config:
+        
+        # Split the line
+        splitWords = i.split(" ")
+        
+        # Hard code the number of antennas, change if other configuration is used
+        numRxAnt = 4
+        numTxAnt = 2
+        
+        # Get the information about the profile configuration
+        if "profileCfg" in splitWords[0]:
+            startFreq = int(float(splitWords[2]))
+            idleTime = int(splitWords[3])
+            rampEndTime = float(splitWords[5])
+            freqSlopeConst = float(splitWords[8])
+            numAdcSamples = int(splitWords[10])
+            numAdcSamplesRoundTo2 = 1;
+            
+            while numAdcSamples > numAdcSamplesRoundTo2:
+                numAdcSamplesRoundTo2 = numAdcSamplesRoundTo2 * 2;
+                
+            digOutSampleRate = int(splitWords[11]);
+            
+        # Get the information about the frame configuration    
+        elif "frameCfg" in splitWords[0]:
+            
+            chirpStartIdx = int(splitWords[1]);
+            chirpEndIdx = int(splitWords[2]);
+            numLoops = int(splitWords[3]);
+            numFrames = int(splitWords[4]);
+            framePeriodicity = int(splitWords[5]);
+
+            
+    # Combine the read data to obtain the configuration parameters           
+    numChirpsPerFrame = (chirpEndIdx - chirpStartIdx + 1) * numLoops
+    configParameters["numDopplerBins"] = numChirpsPerFrame / numTxAnt
+    configParameters["numRangeBins"] = numAdcSamplesRoundTo2
+    configParameters["rangeResolutionMeters"] = (3e8 * digOutSampleRate * 1e3) / (2 * freqSlopeConst * 1e12 * numAdcSamples)
+    configParameters["rangeIdxToMeters"] = (3e8 * digOutSampleRate * 1e3) / (2 * freqSlopeConst * 1e12 * configParameters["numRangeBins"])
+    configParameters["dopplerResolutionMps"] = 3e8 / (2 * startFreq * 1e9 * (idleTime + rampEndTime) * 1e-6 * configParameters["numDopplerBins"] * numTxAnt)
+    configParameters["maxRange"] = (300 * 0.9 * digOutSampleRate)/(2 * freqSlopeConst * 1e3)
+    configParameters["maxVelocity"] = 3e8 / (4 * startFreq * 1e9 * (idleTime + rampEndTime) * 1e-6 * numTxAnt)
+    
+    return configParameters
+   
+# ------------------------------------------------------------------
+
+# Funtion to read and parse the incoming data
+def readAndParseData16xx(Dataport, configParameters):
+    global byteBuffer, byteBufferLength
+
+    # Constants
+    OBJ_STRUCT_SIZE_BYTES = 12
+    BYTE_VEC_ACC_MAX_SIZE = 2**15
+    MMWDEMO_UART_MSG_DETECTED_POINTS = 1
+    MMWDEMO_UART_MSG_RANGE_PROFILE   = 2
+    MMWDEMO_OUTPUT_MSG_AZIMUT_STATIC_HEAT_MAP = 4
+    MMWDEMO_OUTPUT_MSG_RANGE_DOPPLER_HEAT_MAP = 5
+    maxBufferSize = 2**15
+    magicWord = [2, 1, 4, 3, 6, 5, 8, 7]
+    
+    # Initialize variables
+    magicOK = 0 # Checks if magic number has been read
+    dataOK = 0 # Checks if the data has been read correctly
+    frameNumber = 0
+    detObj = {}
+    
+    readBuffer = Dataport.read(Dataport.in_waiting)
+    byteVec = np.frombuffer(readBuffer, dtype = 'uint8')
+    byteCount = len(byteVec)
+    
+    # Check that the buffer is not full, and then add the data to the buffer
+    if (byteBufferLength + byteCount) < maxBufferSize:
+        byteBuffer[byteBufferLength:byteBufferLength + byteCount] = byteVec[:byteCount]
+        byteBufferLength = byteBufferLength + byteCount
+        
+    # Check that the buffer has some data
+    if byteBufferLength > 16:
+        
+        # Check for all possible locations of the magic word
+        possibleLocs = np.where(byteBuffer == magicWord[0])[0]
+
+        # Confirm that is the beginning of the magic word and store the index in startIdx
+        startIdx = []
+        for loc in possibleLocs:
+            check = byteBuffer[loc:loc+8]
+            if np.all(check == magicWord):
+                startIdx.append(loc)
+               
+        # Check that startIdx is not empty
+        if startIdx:
+            
+            # Remove the data before the first start index
+            if startIdx[0] > 0:
+                byteBuffer[:byteBufferLength-startIdx[0]] = byteBuffer[startIdx[0]:byteBufferLength]
+                byteBufferLength = byteBufferLength - startIdx[0]
+                
+            # Check that there have no errors with the byte buffer length
+            if byteBufferLength < 0:
+                byteBufferLength = 0
+                
+            # word array to convert 4 bytes to a 32 bit number
+            word = [1, 2**8, 2**16, 2**24]
+            
+            # Read the total packet length
+            totalPacketLen = np.matmul(byteBuffer[12:12+4],word)
+            
+            # Check that all the packet has been read
+            if (byteBufferLength >= totalPacketLen) and (byteBufferLength != 0):
+                magicOK = 1
+    
+    # If magicOK is equal to 1 then process the message
+    if magicOK:
+        # word array to convert 4 bytes to a 32 bit number
+        word = [1, 2**8, 2**16, 2**24]
+        
+        # Initialize the pointer index
+        idX = 0
+        
+        # Read the header
+        magicNumber = byteBuffer[idX:idX+8]
+        idX += 8
+        version = format(np.matmul(byteBuffer[idX:idX+4],word),'x')
+        idX += 4
+        totalPacketLen = np.matmul(byteBuffer[idX:idX+4],word)
+        idX += 4
+        platform = format(np.matmul(byteBuffer[idX:idX+4],word),'x')
+        idX += 4
+        frameNumber = np.matmul(byteBuffer[idX:idX+4],word)
+        idX += 4
+        timeCpuCycles = np.matmul(byteBuffer[idX:idX+4],word)
+        idX += 4
+        numDetectedObj = np.matmul(byteBuffer[idX:idX+4],word)
+        idX += 4
+        numTLVs = np.matmul(byteBuffer[idX:idX+4],word)
+        idX += 4
+        subFrameNumber = np.matmul(byteBuffer[idX:idX+4],word)
+        idX += 4
+        
+        # Read the TLV messages
+        for tlvIdx in range(numTLVs):
+            
+            # word array to convert 4 bytes to a 32 bit number
+            word = [1, 2**8, 2**16, 2**24]
+
+            # Check the header of the TLV message
+            tlv_type = np.matmul(byteBuffer[idX:idX+4],word)
+            idX += 4
+            tlv_length = np.matmul(byteBuffer[idX:idX+4],word)
+            idX += 4
+            
+            # Read the data depending on the TLV message
+            if tlv_type == MMWDEMO_UART_MSG_DETECTED_POINTS:
+                            
+                # word array to convert 4 bytes to a 16 bit number
+                word = [1, 2**8]
+                tlv_numObj = np.matmul(byteBuffer[idX:idX+2],word)
+                idX += 2
+                tlv_xyzQFormat = 2**np.matmul(byteBuffer[idX:idX+2],word)
+                idX += 2
+                
+                # Initialize the arrays
+                rangeIdx = np.zeros(tlv_numObj,dtype = 'int16')
+                dopplerIdx = np.zeros(tlv_numObj,dtype = 'int16')
+                peakVal = np.zeros(tlv_numObj,dtype = 'int16')
+                x = np.zeros(tlv_numObj,dtype = 'int16')
+                y = np.zeros(tlv_numObj,dtype = 'int16')
+                z = np.zeros(tlv_numObj,dtype = 'int16')
+                
+                for objectNum in range(tlv_numObj):
+                    
+                    # Read the data for each object
+                    rangeIdx[objectNum] =  np.matmul(byteBuffer[idX:idX+2],word)
+                    idX += 2
+                    dopplerIdx[objectNum] = np.matmul(byteBuffer[idX:idX+2],word)
+                    idX += 2
+                    peakVal[objectNum] = np.matmul(byteBuffer[idX:idX+2],word)
+                    idX += 2
+                    x[objectNum] = np.matmul(byteBuffer[idX:idX+2],word)
+                    idX += 2
+                    y[objectNum] = np.matmul(byteBuffer[idX:idX+2],word)
+                    idX += 2
+                    z[objectNum] = np.matmul(byteBuffer[idX:idX+2],word)
+                    idX += 2
+                    
+                # Make the necessary corrections and calculate the rest of the data
+                rangeVal = rangeIdx * configParameters["rangeIdxToMeters"]
+                dopplerIdx[dopplerIdx > (configParameters["numDopplerBins"]/2 - 1)] = dopplerIdx[dopplerIdx > (configParameters["numDopplerBins"]/2 - 1)] - 65535
+                dopplerVal = dopplerIdx * configParameters["dopplerResolutionMps"]
+                #x[x > 32767] = x[x > 32767] - 65536
+                #y[y > 32767] = y[y > 32767] - 65536
+                #z[z > 32767] = z[z > 32767] - 65536
+                x = x / tlv_xyzQFormat
+                y = y / tlv_xyzQFormat
+                z = z / tlv_xyzQFormat
+                
+                # Store the data in the detObj dictionary
+                detObj = {"numObj": tlv_numObj, "rangeIdx": rangeIdx, "range": rangeVal, "dopplerIdx": dopplerIdx, \
+                          "doppler": dopplerVal, "peakVal": peakVal, "x": x, "y": y, "z": z}
+                
+                
+                #print(detObj['range'].mean())
+                
+            elif tlv_type == MMWDEMO_UART_MSG_RANGE_PROFILE:
+                idX += tlv_length
+
+            elif tlv_type == MMWDEMO_OUTPUT_MSG_AZIMUT_STATIC_HEAT_MAP:
+
+                numTxAzimAnt = 2
+                numRxAnt = 4
+                numBytes = numTxAzimAnt * numRxAnt * configParameters["numRangeBins"] * 4;
+
+                q = byteBuffer[idX:idX + numBytes]
+                #print(q)
+
+                idX += numBytes
+                qrows = numTxAzimAnt * numRxAnt
+                qcols = configParameters["numRangeBins"] 
+                NUM_ANGLE_BINS = 64
+
+                real = q[::4] + q[1::2] * 256
+                imaginary = q[2::4] + q[3::2] * 256
+
+                real = real.astype(np.int16)
+                imaginary = imaginary.astype(np.int16)
+
+                q = real + 1j * imaginary
+
+                q = np.reshape(q,(qrows,qcols),order="F")
+
+                Q = np.fft.fft(q,NUM_ANGLE_BINS,axis=0)
+                QQ = np.fft.fftshift(abs(Q),axes=0);
+                QQ = QQ.T
+
+                QQ = QQ[:,1:]
+                QQ = np.fliplr(QQ)
+
+                theta = np.rad2deg(np.arcsin(np.array(range(-NUM_ANGLE_BINS//2+1,NUM_ANGLE_BINS//2))*(2/NUM_ANGLE_BINS)))
+                rangeArray = np.array(range(configParameters["numRangeBins"]))*configParameters["rangeIdxToMeters"]
+
+                posX = np.outer(rangeArray.T,np.sin(np.deg2rad(theta)))
+                posY = np.outer(rangeArray.T,np.cos(np.deg2rad(theta)))
+
+                # Store the data in the azimMapObject dictionary
+                azimMapObject = {"posX": posX, "posY": posY, "range": rangeArray, "theta": theta, "heatMap": QQ}
+                
+                dataOK = 1
+
+
+            elif tlv_type == MMWDEMO_OUTPUT_MSG_RANGE_DOPPLER_HEAT_MAP:
+                numBytes = configParameters["numDopplerBins"] * configParameters["numRangeBins"] * 2
+                rangeDoppler = byteBuffer[idX:idX + numBytes]
+
+                rangeDoppler = rangeDoppler[::2] + rangeDoppler[1::2]*256
+
+                rangeDoppler = np.reshape(rangeDoppler,(configParameters["numDopplerBins"],configParameters["numRangeBins"]))
+                rangeDoppler = np.concatenate((byteBuffer[(byteBufferLength + 1)//2:],byteBuffer[:(byteBufferLength + 1)//2]))
+
+                range = math.dotMultiply(math.range(0, Params.dataPath[subFrameNum].numRangeBins - 1, true), Params.dataPath[subFrameNum].rangeIdxToMeters);
+        
+                # NOT FINISHED
+                
+        
+        # Remove already processed data
+        if idX > 0 and dataOK == 1:
+            shiftSize = idX
+            
+                
+            byteBuffer[:byteBufferLength - shiftSize] = byteBuffer[shiftSize:byteBufferLength]
+            byteBufferLength = byteBufferLength - shiftSize
+            
+            # Check that there are no errors with the buffer length
+            if byteBufferLength < 0:
+                byteBufferLength = 0
+                
+
+    return dataOK, frameNumber, detObj, azimMapObject
+
+# ------------------------------------------------------------------
+
+# Funtion to update the data and display in the plot
+def update():
+     
+    dataOk = 0
+    global detObj
+    global azimMapObject
+    x = []
+    y = []
+      
+    # Read and parse the received data
+    dataOk, frameNumber, detObj, azimMapObject = readAndParseData16xx(Dataport, configParameters)
+    
+    if dataOk:
+        plt.clf()
+
+        ## 2D POSITION HEAT MAP
+        ax.contourf(azimMapObject["posX"],azimMapObject["posY"],azimMapObject["heatMap"])
+
+        ## ANGLE VS. RANGE HEATMAP
+        #X,Y = np.meshgrid(azimMapObject["theta"], azimMapObject["range"])
+        #ax.contourf(X,Y,azimMapObject["heatMap"])
+
+        fig.canvas.draw()
+        plt.pause(0.25)
+    
+    return dataOk
+
+
+# -------------------------    MAIN   -----------------------------------------  
+
+# Configurate the serial port
+CLIport, Dataport = serialConfig(configFileName)
+
+# Get the configuration parameters from the configuration file
+configParameters = parseConfigFile(configFileName)
+
+fig = plt.figure()
+
+# Main loop 
+detObj = {}  
+azimMapObject = {}
+frameData = {}    
+currentIndex = 0
+while True:
+    try:
+        # Update the data and check if the data is okay
+        dataOk = update()
+        
+        if dataOk:
+            # Store the current frame into frameData
+            frameData[currentIndex] = detObj
+            currentIndex += 1
+        
+        
+    # Stop the program and close everything if Ctrl + c is pressed
+    except KeyboardInterrupt:
+        CLIport.write(('sensorStop\n').encode())
+        CLIport.close()
+        Dataport.close()
+        win.close()
+        break
+        
+    
+
+
+
+
+
